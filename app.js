2c582a44108a04a9fd6b7d1a9227e6ea10d54555
KingDavid2908 <103088635+KingDavid2908@users.noreply.github.com>
2024-04-23 00:40:43 +0100

    I made the navbar link change to different sections with event listeners and the menubar to be removed after it has been clicked. I used forEach with querySelectorAll.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".navbar__menu");
-const menuLinksClicked = document.querySelector(".navbar__links");
+const menuLinksClicked = document.querySelectorAll(".navbar__links");
 
 const menu__toggle = () => {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
+  i;
 };
 
 menu.addEventListener("click", menu__toggle);
 
-menuLinksClicked.addEventListener("click", menu__toggle);
+menuLinksClicked.forEach((menuLinkClicked) => {
+  menuLinkClicked.addEventListener("click", menu__toggle);
+});
